e6644f419c2ac41434783a6ba1107178224a46b3
Evgenii Khramkov <evkhramk@microsoft.com>
2019-02-05 10:46:52 +0300

    Initial Commit

---
+++ ios-native-application-stores/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-native-application-stores
+//
+//  Created by Evgenii Khramkov on 2/5/19.
+//  Copyright © 2019 ewgeniux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
